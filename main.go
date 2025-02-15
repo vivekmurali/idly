a4ed47c1f683c1203044b7177607847c26adfc24
Vivek Murali <vivekmurali2k@gmail.com>
2025-02-14 23:38:24 -0500

    Added Read SSTable and Get value from key

--- main.go
+++ main.go
@@ -12,7 +12,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	log.Println("Completed writing to SS Table")
+
+	sst, err := Read("ss_1.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Read from SSTable")
+
+	val1, err := sst.Get("abc")
+	if err != nil {
+		log.Fatal(err)
+	}
+	val2, err := sst.Get("def")
+	if err != nil {
+		log.Fatal(err)
+	}
+	val3, err := sst.Get("ghijk")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("abc: ", val1)
+	log.Println("def: ", val2)
+	log.Println("ghijk: ", val3)
 
 }
--- sstable.go
+++ sstable.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/binary"
+	"errors"
 	"os"
 	"sort"
 )
@@ -33,17 +34,14 @@ func Write(filename string, kvs []KeyValue) error {
 
 	writer := bufio.NewWriter(file)
 
+	var curOffset int64
 	for _, kv := range kvs {
-		offset, err := file.Seek(0, os.SEEK_CUR)
-		if err != nil {
-			return err
-		}
-		index[kv.Key] = offset
-
+		index[kv.Key] = curOffset
 		binary.Write(writer, binary.LittleEndian, int32(len(kv.Key)))
 		writer.WriteString(kv.Key)
 		binary.Write(writer, binary.LittleEndian, int32(len(kv.Value)))
 		writer.WriteString(kv.Value)
+		curOffset += 8 + int64(len(kv.Key)) + int64(len(kv.Value))
 	}
 	writer.Flush()
 
@@ -62,4 +60,59 @@ func Write(filename string, kvs []KeyValue) error {
 	writer.Flush()
 
 	return nil
+}
+
+func Read(filename string) (*SSTable, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	file.Seek(-8, os.SEEK_END)
+	var indexOffset int64
+	err = binary.Read(file, binary.LittleEndian, &indexOffset)
+	if err != nil {
+		return nil, err
+	}
+	file.Seek(indexOffset, os.SEEK_SET)
+	index := make(map[string]int64)
+
+	for {
+		var keyLen int32
+		err = binary.Read(file, binary.LittleEndian, &keyLen)
+		if err != nil {
+			break
+		}
+		key := make([]byte, keyLen)
+		file.Read(key)
+		var offset int64
+		binary.Read(file, binary.LittleEndian, &offset)
+		index[string(key)] = offset
+	}
+
+	return &SSTable{filename, index}, nil
+}
+
+func (sst *SSTable) Get(key string) (string, error) {
+	val, ok := sst.index[key]
+	if !ok {
+		return "", errors.New("Key not found")
+	}
+	file, err := os.Open(sst.filename)
+	if err != nil {
+		return "", err
+	}
+	file.Seek(val, os.SEEK_SET)
+	var keyLen int32
+	binary.Read(file, binary.LittleEndian, &keyLen)
+	keyByte := make([]byte, keyLen)
+	file.Read(keyByte)
+
+	var valLen int32
+	binary.Read(file, binary.LittleEndian, &valLen)
+	value := make([]byte, valLen)
+	file.Read(value)
+
+	return string(value), nil
 }
